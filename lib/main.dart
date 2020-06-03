75b6d40be56e2149e37c2e2185a31dae90b84cf1
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-06-03 23:56:17 +0530

    cloud notifications with background working

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_news_app/src/ui/app.dart';
+import 'package:flutter_news_app/src/notifications/messageHandler.dart';
+// import 'package:flutter_news_app/src/ui/app.dart';
 // import 'package:flutter_stetho/flutter_stetho.dart';
 
 void main() {
   // Stetho.initialize();
-  runApp(App());
+  runApp(MessageHandler());
 }
---
+++ lib/src/notifications/messageHandler.dart
@@ -0,0 +1,94 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_news_app/src/ui/app.dart';
+
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
+  return Future<void>.value();
+}
+
+class MessageHandler extends StatefulWidget {
+  @override
+  _MessageHandlerState createState() => _MessageHandlerState();
+}
+
+class _MessageHandlerState extends State<MessageHandler> {
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+
+  @override
+  void initState() {
+    _fcm.configure(
+      onBackgroundMessage: Theme.of(context).platform == TargetPlatform.iOS
+          ? null
+          : myBackgroundMessageHandler,
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message['notification']['title']),
+              subtitle: Text(message['notification']['body']),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message['notification']['title']),
+              subtitle: Text(message['notification']['body']),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message['notification']['title']),
+              subtitle: Text(message['notification']['body']),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return App();
+  }
+
+  _subscribeToTopic() async {
+    // Subscribe the user to a topic
+    _fcm.subscribeToTopic('puppies');
+  }
+}
